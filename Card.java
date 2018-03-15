cf67c90a77da80df2e9ee90c52f40c635c4b37e7
pavlosp1967 <pavlosp1967@gmail.com>
2018-03-14 23:41:18 -0700

    Update Card and Hand

--- Card.java
+++ Card.java
@@ -2,190 +2,179 @@
  * The Class Card.
  * @author Hunter Mason, Jake McGhee, Mac Dousias, Pavlos Papadonikolakis
  */
-public class Card implements Cloneable {
-
-	/**
-	 *  Enumerated Suit values
-	 */
-	public enum Suit {
-		clubs, diamonds, hearts, spades
-	}
-
-	/** card value. */
-	private char value;
-
-	/** card suit. */
-	private Suit suit;
-
-	/** error flag, keeps track of invalid entries */
-	private boolean errorFlag;
-
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 *            
-	 */
-	public static void main(String[] args) {
-		
-		//testing three cards two valid and one invalid
-		Card c1 = new Card('A', Suit.spades);
-		Card c2 = new Card('s', Suit.spades);
-		Card c3 = new Card('J', Suit.clubs);
-
-		System.out.println(c1.toString());
-		System.out.println(c2.toString());
-		System.out.println(c3.toString());
-
-		System.out.println();
-		
-		//testing three more cards two valid and one invalid
-		Card c4 = new Card('d', Suit.spades);
-		Card c5 = new Card('Q', Suit.spades);
-		Card c6 = new Card('J', Suit.clubs);
-
-		System.out.println(c4.toString());
-		System.out.println(c5.toString());
-		System.out.println(c6.toString());
-
-	}
-
-	/**
-	 * Instantiates a new card.
-	 */
-	public Card() {
-		this.value = 'A';
-		this.suit = Suit.spades;
-	}
-
-	/**
-	 * Instantiates a new card, by calling the set() method
-	 *
-	 * @param value  the value
-	 *           
-	 * @param suit the suit
-	 *            
-	 */
-	public Card(char value, Suit suit) {
-		set(value, suit);
-	}
-
-	/**
-	 * returns a cloned object
-	 */
-	public Object clone() {
-
-		try {
-			return super.clone();
-		} catch (CloneNotSupportedException e) {
-
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	/**
-	 * Sets the value and the suit
-	 *
-	 * @param value the value
-	 *            
-	 * @param suit the suit
-	 *            
-	 * @return true, if successful
-	 */
-	public boolean set(char value, Suit suit) {
-
-		if (isValid(value, suit)) {
-			this.suit = suit;
-			this.value = value;
-			errorFlag = false;
-			return true;
-		}
-		errorFlag = true;
-		return false;
-
-	}
-
-	/**
-	 * Checks if is the values entered by the user are valid.
-	 *
-	 * @param value the value
-	 *            
-	 * @param suit the suit
-	 *            
-	 * @return true, if is valid
-	 */
-	public boolean isValid(char value, Suit suit) {
-		return (value == 'A' || value == 'K' || value == 'Q' || value == 'J' || value == 'T'
-				|| (value >= '2' && value <= '9'))
-				&& (suit == Suit.clubs || suit == Suit.diamonds || suit == Suit.hearts || suit == Suit.spades);
-	}
-
-	/**
-	 * prints the cards
-	 */
-	public String toString() {
-		if (errorFlag == true) {
-			return "** illegal **";
-		} else {
-
-			String retVal = String.valueOf(getValue());
-
-			if (suit == Suit.spades)
-				retVal += " of Spades";
-			else if (suit == Suit.hearts)
-				retVal += " of Hearts";
-			else if (suit == Suit.diamonds)
-				retVal += " of Diamonds";
-			else if (suit == Suit.clubs)
-				retVal += " of Clubs";
-			return retVal;
-		}
-	}
-
-	/**
-	 * Gets the error flag.
-	 *
-	 * @return the error flag
-	 */
-	public boolean getErrorFlag() {
-		return errorFlag;
-	}
-
-	/**
-	 * Gets the suit.
-	 *
-	 * @return the suit
-	 */
-	public Suit getSuit() {
-		return this.suit;
-	}
-
-	/**
-	 * Gets the value.
-	 *
-	 * @return the value
-	 */
-	public char getValue() {
-		return value;
-	}
-
-	/**
-	 * checks for equal values
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		final Card other = (Card) obj;
-		if (this == obj) {
-			return true;
-		}
-
-		if (obj == null || getClass() != obj.getClass() || this.value != other.value
-				|| this.errorFlag != other.errorFlag || this.suit != other.suit) {
-			return false;
-		}
-
-		return true;
-	}
-
+public class Card {
+    
+    /**
+     *  Enumerated Suit values
+     */
+    public enum Suit {
+        clubs, diamonds, hearts, spades
+    }
+    
+    /** card value. */
+    private char value;
+    
+    /** card suit. */
+    private Suit suit;
+    
+    /** error flag, keeps track of invalid entries */
+    private boolean errorFlag;
+    
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     *
+     */
+    public static void main(String[] args) {
+        
+        //testing three cards two valid and one invalid
+        Card c1 = new Card('A', Suit.spades);
+        Card c2 = new Card('s', Suit.spades);
+        Card c3 = new Card('J', Suit.clubs);
+        
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        System.out.println(c3.toString());
+        
+        System.out.println();
+        
+        //testing three more cards two valid and one invalid
+        Card c4 = new Card('d', Suit.spades);
+        Card c5 = new Card('Q', Suit.spades);
+        Card c6 = new Card('J', Suit.clubs);
+        
+        System.out.println(c4.toString());
+        System.out.println(c5.toString());
+        System.out.println(c6.toString());
+        
+    }
+    
+    /**
+     * Instantiates a new card.
+     */
+    public Card() {
+        this.value = 'A';
+        this.suit = Suit.spades;
+    }
+    
+    /**
+     * Instantiates a new card, by calling the set() method
+     *
+     * @param value  the value
+     *
+     * @param suit the suit
+     *
+     */
+    public Card(char value, Suit suit) {
+        set(value, suit);
+    }
+    
+    /**
+     * Sets the value and the suit
+     *
+     * @param value the value
+     *
+     * @param suit the suit
+     *
+     * @return true, if successful
+     */
+    public boolean set(char value, Suit suit) {
+        
+        if (!isValid(value, suit)) {
+            errorFlag = true;
+            return false;
+            
+        }
+        this.suit = suit;
+        this.value = value;
+        errorFlag = false;
+        return true;
+        
+        
+    }
+    
+    /**
+     * Checks if is the values entered by the user are valid.
+     *
+     * @param value the value
+     *
+     * @param suit the suit
+     *
+     * @return true, if is valid
+     */
+    public boolean isValid(char value, Suit suit) {
+        return (value == 'A' || value == 'K' || value == 'Q' || value == 'J' || value == 'T'
+                || (value >= '2' && value <= '9'))
+        && (suit == Suit.clubs || suit == Suit.diamonds || suit == Suit.hearts || suit == Suit.spades);
+    }
+    
+    /**
+     * prints the cards
+     */
+    public String toString() {
+        if (errorFlag == true) {
+            return "** illegal **";
+        } else {
+            
+            String retVal = String.valueOf(getValue());
+            
+            if (suit == Suit.spades)
+                retVal += " of Spades";
+            else if (suit == Suit.hearts)
+                retVal += " of Hearts";
+            else if (suit == Suit.diamonds)
+                retVal += " of Diamonds";
+            else if (suit == Suit.clubs)
+                retVal += " of Clubs";
+            return retVal;
+        }
+    }
+    
+    /**
+     * Gets the error flag.
+     *
+     * @return the error flag
+     */
+    public boolean getErrorFlag() {
+        return errorFlag;
+    }
+    
+    /**
+     * Gets the suit.
+     *
+     * @return the suit
+     */
+    public Suit getSuit() {
+        return this.suit;
+    }
+    
+    /**
+     * Gets the value.
+     *
+     * @return the value
+     */
+    public char getValue() {
+        return value;
+    }
+    
+    /**
+     * checks for equal values
+     */
+    @Override
+    public boolean equals(Object obj) {
+        final Card other = (Card) obj;
+        if (this == obj) {
+            return true;
+        }
+        
+        if (obj == null || getClass() != obj.getClass() || this.value != other.value
+            || this.errorFlag != other.errorFlag || this.suit != other.suit) {
+            return false;
+        }
+        
+        return true;
+    }
+    
 }
+
--- Hand.java
+++ Hand.java
@@ -1,24 +1,26 @@
 /**
- * The Class Hand 
+ * The Class Hand
+ * 
  * @author Hunter Mason, Jake McGhee, Mac Dousias, Pavlos Papadonikolakis
- *  
+ * 
  */
-public class Hand extends Card {
-	
+public class Hand  {
+
 	/**
 	 * The main method.
 	 *
 	 * @param args the arguments
+	 *            
 	 */
 	public static void main(String[] args) {
 
 		// creating a hand object and 4 card objects
 		Hand hand1 = new Hand();
 
-		Card card1 = new Card('2', Suit.diamonds);
-		Card card2 = new Card('4', Suit.hearts);
-		Card card3 = new Card('5', Suit.spades);
-		Card card4 = new Card('T', Suit.clubs);
+		Card card1 = new Card('2',  Card.Suit.diamonds);
+		Card card2 = new Card('4',  Card.Suit.hearts);
+		Card card3 = new Card('5',  Card.Suit.spades);
+		Card card4 = new Card('T',  Card.Suit.clubs);
 
 		// populate the hand with legal and
 		// illegal values
@@ -26,39 +28,37 @@ public class Hand extends Card {
 			hand1.takeCard(card1);
 			hand1.takeCard(card2);
 			hand1.takeCard(card3);
-			card3.set('e', Suit.diamonds);
+			card3.set('e',  Card.Suit.diamonds);
 			hand1.takeCard(card3);
+			card4.set('w',  Card.Suit.diamonds);
 			hand1.takeCard(card4);
-			card4.set('w', Suit.diamonds);
+			
 		}
 		System.out.println(hand1.toString());
 
 		// inspect Card at last index (first inserted)
 		System.out.println("\nTesting inspectCard()");
 		System.out.println(hand1.inspectCard(0));
-		//change the last card to illegal
-		hand1.myCards[49]=new Card('s', Suit.diamonds);
-		Card test = hand1.inspectCard(49);
+		// inspect illegal index 
+		Card test = hand1.inspectCard(50);
 		System.out.println(test);
 
-		//print all individual cards
+		// print all individual cards
 		while (hand1.getNumOfCards() != 0) {
 			Card c = hand1.playCard();
-			if (c != null) {
 				System.out.println("Playing " + c);
-			}
 		}
-		 System.out.println("\nAfter playing all cards");
-		 System.out.println(hand1.toString());
+		System.out.println("\nAfter playing all cards");
+		System.out.println(hand1.toString());
 
 	}
 
 	/** The max cards in the hand */
-	public int MAX_CARDS = 52;
-	
+	public int MAX_CARDS = 50;
+
 	/** holds all the cards */
 	Card[] myCards = new Card[MAX_CARDS];
-	
+
 	/** The number of card in the array. */
 	int numCards;
 
@@ -80,17 +80,14 @@ public class Hand extends Card {
 	/**
 	 * Adds a card to the next available position in the myCards array
 	 *
-	 * @param card the card
+	 * @param card
+	 *            the card
 	 * @return true, if successful
 	 */
 	public boolean takeCard(Card card) {
-		
-		Card copyCard = card;
-		char value = copyCard.getValue();
-		Suit suit = copyCard.getSuit();
 
 		if (numCards < MAX_CARDS) {
-			myCards[numCards++] = new Card(value, suit);
+			myCards[numCards++] = new Card(card.getValue(), card.getSuit());
 
 			if (numCards == MAX_CARDS) {
 				System.out.println("\nHand Full\nAfter Deal");
@@ -101,21 +98,17 @@ public class Hand extends Card {
 	}
 
 	/**
-	 * Returns and removes the card in the
-	 * top occupied position of the array
+	 * Returns and removes the card in the top occupied position of the array
 	 *
 	 * @return a deep copy of the card
 	 */
 	Card playCard() {
+		Card error = new Card('w', Card.Suit.spades);
+		if (numCards == 0)
+			return error;
 
-		Card[] copy = new Card[MAX_CARDS];
-		if (!myCards[numCards - 1].getErrorFlag()) {
-			copy[numCards - 1] = (Card) myCards[numCards - 1].clone();
-		}
-
-		return copy[--numCards];
+		return myCards[--numCards];
 	}
-
 
 	/**
 	 * prints the cards in the hand
@@ -131,18 +124,7 @@ public class Hand extends Card {
 			String retVal = "\nHand = ( ";
 
 			for (int i = 0; i < numCards; i++) {
-				if (!getErrorFlag())
-					retVal += myCards[i].getValue();
-
-				if (myCards[i].getSuit().equals(Suit.spades))
-					retVal += " of Spades";
-				else if (myCards[i].getSuit().equals(Suit.hearts))
-					retVal += " of Hearts";
-				else if (myCards[i].getSuit().equals(Suit.diamonds))
-					retVal += " of Diamonds";
-				else if (myCards[i].getSuit().equals(Suit.clubs))
-					retVal += " of Clubs";
-
+				retVal += myCards[i].toString();
 				if (counter + 1 != numCards)
 					retVal += ",";
 
@@ -167,14 +149,17 @@ public class Hand extends Card {
 	}
 
 	/**
-	 * Accessor for an individual card.
-	 * Returns a card with errorFlag = true if k is bad
+	 * Accessor for an individual card. Returns a card with errorFlag = true if
+	 * k is bad
 	 *
-	 * @param k the index of the card in the array
+	 * @param k
+	 *            the index of the card in the array
 	 * @return the card
 	 */
 	Card inspectCard(int k) {
-		assert (k >= 0 && k < numCards && numCards > 0);
+		Card error = new Card('w', Card.Suit.spades);
+		if (k < 0 || k >= numCards)
+			return error;
 		return myCards[k];
 
 	}
